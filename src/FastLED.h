297f8074461a81d8225fce4723df5f440808e392
Drollbaer <marcosax95@web.de>
2023-05-19 14:30:25 +0200

    fix bool 1

--- src/FastLED.h
+++ src/FastLED.h
@@ -371,7 +371,7 @@ public:
 	/// Drollbaer: 19.06.2023
 	/// Add a clockless based CLEDController instance to the world.
 	template<template<uint8_t DATA_PIN, EOrder RGB_ORDER> class CHIPSET, uint8_t DATA_PIN, EOrder RGB_ORDER>
-	static CLEDController &addLedsR(struct CRGB *data, int nLedsOrOffset, int nLedsIfOffset = 0, bool bReverse=false) {
+	static CLEDController &addLedsR(struct CRGB *data, int nLedsOrOffset, int nLedsIfOffset = 0, bool bReverse) {
 		static CHIPSET<DATA_PIN, RGB_ORDER> c;
 		return addLedsR(&c, data, nLedsOrOffset, nLedsIfOffset, bReverse);
 	}
